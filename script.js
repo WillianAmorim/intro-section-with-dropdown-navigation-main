af1c195f7957e8e05ba0c9ebd98ca84f0e7c4794
Willian <willian.amorim1@outlook.com>
2023-02-24 19:42:19 -0300

    finalizado estrutura do header

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const iconsHeader = document.querySelectorAll('.click-header')
+// const iconsHeader = document.querySelectorAll('.click-header')
 
-iconsHeader.forEach((element) => {
-  element.addEventListener('click', () => {
-    const imagem = element.children[0];
+// iconsHeader.forEach((element) => {
+//   element.addEventListener('click', () => {
+//     const imagem = element.children[0];
 
-    if (imagem.src.endsWith('icon-arrow-down.svg')) {
-      imagem.src = './images/icon-arrow-up.svg';
-      element.children[1].classList.remove('hidden-list')
-      element.children[1].classList.add('show-list')
-    } else {
-      imagem.src = './images/icon-arrow-down.svg';
-      element.children[1].classList.remove('show-list')
-      element.children[1].classList.add('hidden-list')
-    }
-  });
-})
+//     if (imagem.src.endsWith('icon-arrow-down.svg')) {
+//       imagem.src = './images/icon-arrow-up.svg';
+//       element.children[1].classList.remove('hidden-list')
+//       element.children[1].classList.add('show-list')
+//     } else {
+//       imagem.src = './images/icon-arrow-down.svg';
+//       element.children[1].classList.remove('show-list')
+//       element.children[1].classList.add('hidden-list')
+//     }
+//   });
+// })
