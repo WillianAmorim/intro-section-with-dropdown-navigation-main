7ca4a305361e8f442aae8d456166c1c5fe744c57
Willian <willian.amorim1@outlook.com>
2023-02-24 19:11:45 -0300

    adicionado função para mostrar div e ocultar div

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ iconsHeader.forEach((element) => {
 
     if (imagem.src.endsWith('icon-arrow-down.svg')) {
       imagem.src = './images/icon-arrow-up.svg';
+      element.children[1].classList.remove('hidden-list')
+      element.children[1].classList.add('show-list')
     } else {
       imagem.src = './images/icon-arrow-down.svg';
+      element.children[1].classList.remove('show-list')
+      element.children[1].classList.add('hidden-list')
     }
   });
 })
