7bd7eaa15fce8425d2888285e5f7ada0ad2f3227
Willian <willian.amorim1@outlook.com>
2023-02-25 00:30:37 -0300

    finalizado evento de click do header

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-// const iconsHeader = document.querySelectorAll('.click-header')
+const iconsHeader = document.querySelectorAll('.click-header')
 
-// iconsHeader.forEach((element) => {
-//   element.addEventListener('click', () => {
-//     const imagem = element.children[0];
+iconsHeader.forEach((element) => {
+  element.addEventListener('click', () => {
+    const imagem = element.children[1];
 
-//     if (imagem.src.endsWith('icon-arrow-down.svg')) {
-//       imagem.src = './images/icon-arrow-up.svg';
-//       element.children[1].classList.remove('hidden-list')
-//       element.children[1].classList.add('show-list')
-//     } else {
-//       imagem.src = './images/icon-arrow-down.svg';
-//       element.children[1].classList.remove('show-list')
-//       element.children[1].classList.add('hidden-list')
-//     }
-//   });
-// })
+    if (imagem.src.endsWith('icon-arrow-down.svg')) {
+      imagem.src = './images/icon-arrow-up.svg';
+      element.nextElementSibling.classList.remove('list-hidden')
+      element.nextElementSibling.classList.add('list-show')
+    } else {
+      imagem.src = './images/icon-arrow-down.svg';
+      element.nextElementSibling.classList.remove('list-show')
+      element.nextElementSibling.classList.add('list-hidden')
+    }
+  });
+})
+
+
+
+
