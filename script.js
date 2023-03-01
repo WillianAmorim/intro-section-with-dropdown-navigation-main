2360b3413e12a8a651d64c96abc7da9904a37016
Willian <willian.amorim1@outlook.com>
2023-03-01 20:51:06 -0300

    adicionado icones do menu e nav oculta

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-const iconsHeader = document.querySelectorAll('.click-header')
+// const iconsHeader = document.querySelectorAll('.click-header')
 
-iconsHeader.forEach((element) => {
-  element.addEventListener('click', () => {
-    const imagem = element.children[1];
+// iconsHeader.forEach((element) => {
+//   element.addEventListener('click', () => {
+//     const imagem = element.children[1];
 
-    if (imagem.src.endsWith('icon-arrow-down.svg')) {
-      imagem.src = './images/icon-arrow-up.svg';
-      element.nextElementSibling.classList.remove('list-hidden')
-      element.nextElementSibling.classList.add('list-show')
-    } else {
-      imagem.src = './images/icon-arrow-down.svg';
-      element.nextElementSibling.classList.remove('list-show')
-      element.nextElementSibling.classList.add('list-hidden')
-    }
-  });
-})
+//     if (imagem.src.endsWith('icon-arrow-down.svg')) {
+//       imagem.src = './images/icon-arrow-up.svg';
+//       element.nextElementSibling.classList.remove('list-hidden')
+//       element.nextElementSibling.classList.add('list-show')
+//     } else {
+//       imagem.src = './images/icon-arrow-down.svg';
+//       element.nextElementSibling.classList.remove('list-show')
+//       element.nextElementSibling.classList.add('list-hidden')
+//     }
+//   });
+// })
 
-const iconMenu = document.querySelector('.icon-menu')
+// const iconMenu = document.querySelector('.icon-menu')
 
-iconMenu.addEventListener('click', () => {
-  if(iconMenu.src.endsWith('icon-menu.svg')){
-    iconMenu.src = './images/icon-close-menu.svg'
+// iconMenu.addEventListener('click', () => {
+//   if(iconMenu.src.endsWith('icon-menu.svg')){
+//     iconMenu.src = './images/icon-close-menu.svg'
 
-    iconMenu.nextElementSibling.classList.remove('div-hidden')
+//     iconMenu.nextElementSibling.classList.remove('div-hidden')
 
-  } else {
-    iconMenu.src = './images/icon-menu.svg'
-    iconMenu.nextElementSibling.classList.add('div-hidden')
-  }
-})
+//   } else {
+//     iconMenu.src = './images/icon-menu.svg'
+//     iconMenu.nextElementSibling.classList.add('div-hidden')
+//   }
+// })
+
+const clickMenuOpen = () => {
+  nav.style.display = 'flex';
+  menu.style.display = 'none'
+}
+
+const clickMenuClose = () => {
+  nav.style.display = 'none'
+  menu.style.display = 'flex'
+
+}
 
 
 
